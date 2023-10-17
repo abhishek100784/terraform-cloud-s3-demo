367f85e6eff58a7e44b2d238a2a4fe74e3cf462f
abhishek100784 <abhishek.aggarwal@enghouse.com>
2023-10-17 19:12:54 +0200

    Updated Modules

--- c3-s3bucket.tf
+++ c3-s3bucket.tf
@@ -1,7 +1,7 @@
 # Call our Custom Terraform Module which we built earlier
 module "website_s3_bucket" {
   source  = "app.terraform.io/hcta-demo1-abhishek/s3-website/aws"
-  version = "1.0.1"
+  version = "1.0.2"
   # insert required variables here
   bucket_name = var.my_s3_bucket
   tags        = var.my_s3_tags
